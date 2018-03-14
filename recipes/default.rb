67179b83f6d1545f3bc5dc812312a8394c2205ef
Frank Manna <frank.manna@voxmedia.com>
2018-03-14 10:34:46 -0400

    Initial Commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,12 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+node.default['haproxy']['members'] = [
+  {
+    'hostname' => 'ec2-54-174-79-5.compute-1.amazonaws.com',
+	'ipaddress' => '54.174.79.5',
+	'port' => 80,
+	'ssl_port' => 80
+  }]
+
+include_recipe 'haproxy::manual'
