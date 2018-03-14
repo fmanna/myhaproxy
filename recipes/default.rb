6fdd5ed45c0b4decea165c37ee9c0896334c97f5
Frank Manna <frank.manna@voxmedia.com>
2018-03-14 14:02:08 -0400

    Version 0.3.0, added dynamic hostname and IP settings for all web_server role nodes

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,18 +3,21 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-node.default['haproxy']['members'] = [
-  {
-    'hostname' => 'ec2-54-174-79-5.compute-1.amazonaws.com',
-	'ipaddress' => '54.174.79.5',
+all_web_nodes = search('node','role:web_server')
+
+members = []
+
+all_web_nodes.each do |web_node|
+  member = {
+    'hostname' => web_node['cloud']['public_hostname'],
+	'ipaddress' => web_node['cloud']['public_ipv4'],
 	'port' => 80,
 	'ssl_port' => 80
-  },
-  {
-    'hostname' => 'ec2-54-90-115-167.compute-1.amazonaws.com',
-	'ipaddress' => '54.90.115.167',
-	'port' => 80,
-	'ssl_port' => 80
-  }]
+  }
+  
+  members.push(member)
+end
+
+node.default['haproxy']['members'] = members
 
 include_recipe 'haproxy::manual'
